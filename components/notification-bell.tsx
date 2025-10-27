2242012ba8b9bbc852fe049ee419fb55bf679f8b
rmunoz392 <munozrenz12@gmail.com>
2025-10-26 19:09:36 -0600

    feat: implement display for notification item

--- components/notification-bell.tsx
+++ components/notification-bell.tsx
@@ -24,7 +24,7 @@ export function NotificationBell() {
       message: "Possible match found for your lost item", 
       type: "in_app",
       link: "url-to-item",
-      is_read: true,
+      is_read: false,
       created_at: new Date(Date.now() - 2 * 60 * 1000).toISOString() // 2 min ago
     },
     { 
@@ -64,8 +64,22 @@ export function NotificationBell() {
       <DropdownMenuContent align="end" className="w-80"> 
         <DropdownMenuLabel>Notifications</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        {notifications.length === 0 && (
+        {/* 
+            If there are no notifications display a message indicating so,
+            otherwise display the notifications dropdown items 
+        */}
+        {notifications.length === 0 ? (
           <DropdownMenuItem>No new notifications</DropdownMenuItem>
+        ) : (
+          notifications.map(notification => (
+            <DropdownMenuItem key={notification.id}>
+              {!notification.is_read && (<span className="h-2 w-2 bg-blue-500 rounded-full ml-2 mt-1.5" />)}
+              {notification.message}
+              <span className="text-xs text-muted-foreground mr-2">
+              {new Date(notification.created_at).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+            </span>
+            </DropdownMenuItem>
+          ))
         )}
         <DropdownMenuSeparator />
         <DropdownMenuItem className="text-center justify-center cursor-pointer">
