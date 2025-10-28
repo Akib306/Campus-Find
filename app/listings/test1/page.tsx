91d4a6a020916d6f54f828922a51db8da736730c
rmunoz392 <munozrenz12@gmail.com>
2025-10-28 16:02:08 -0600

    style: center text in test listing page

--- app/listings/test1/page.tsx
+++ app/listings/test1/page.tsx
@@ -1,9 +1,7 @@
 export default function ListingPage({ params }: { params: { id: string } }) {
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">Test Listing</h1>
-      <p>Listing ID: {params.id}</p>
-      <p>Test for deep linking.</p>
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold ">Test Listing for notification Deeplink</h1>
     </div>
   );
 }
