98bd3d92e080bbb8edee9b54ca471ab18b072053
rmunoz392 <munozrenz12@gmail.com>
2025-10-28 16:19:50 -0600

    refactor: type defaults to 'in_app

--- supabase/migrations/20251026220150_create_notifications_table.sql
+++ supabase/migrations/20251026220150_create_notifications_table.sql
@@ -3,7 +3,7 @@ create table if not exists public.notifications (
     id uuid     default gen_random_uuid() primary key,          -- Unique notification ID, for each unique notification to a user
     user_id     uuid references auth.users(id) not null,        -- ID of the user receiving the notification
     message     text not null,                                  -- Notification message content
-    type        text not null,                                  -- Type of notification: 'in_app', 'email', or 'both'
+    type        text default 'in_app',                          -- Type of notification: 'in_app', 'email', or 'both' (defaults to 'in_app')
     link        text,                                           -- Deep link to the relevant listing, depending on the notification type
     is_read     boolean default false,                          -- Flag to indicate if the notification has been read
     created_at  timestamptz not null default now()              -- Timestamp when the notification was created
