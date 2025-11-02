dbc77f1dc0e7e85383edd6fd3723060d983ad842
Akib306 <123131218+Akib306@users.noreply.github.com>
2025-11-02 11:58:20 -0600

    revert: changes from the merge

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,14 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-  console.log(
-    "Supabase Key:",
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY?.slice(0, 10) + "..."
-  );
-
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
   );
 }
