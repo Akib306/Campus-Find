e4ab5dd486646e184c636c1e548851903ab45393
Akib306 <123131218+Akib306@users.noreply.github.com>
2025-11-13 01:37:26 -0600

    style: improve navbar layout with grid and increase search width

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,22 +10,24 @@ import { hasEnvVars } from "@/lib/utils";
 export async function Navbar() {
   return (
     <header className="sticky top-0 z-50 w-full bg-background/80 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <nav className="mx-auto w-[80%]  flex h-14 items-center gap-3 px-4 sm:px-6 lg:px-8 ">
-        <div className="flex items-center justify-between w-full">
-          <>
-            <Link href="/" className="text-sm font-semibold tracking-tight">
-              CampusFind
-            </Link>
-            <Search />
-            {!hasEnvVars ? <EnvVarWarning /> : (
-              <>
-                <div className="flex items-center">
-                  <NotificationBell/>
-                  <AuthButton />
-                </div>
-              </>
-            )}
-          </>
+      <nav className="mx-auto w-[80%] grid grid-cols-3 h-14 items-center gap-3 pt-2 sm:px-6 lg:px-8">
+        <div className="justify-self-start">
+          <Link href="/" className="text-sm font-semibold tracking-tight">
+            CampusFind
+          </Link>
+        </div>
+        <div className="justify-self-center w-full">
+          <Search />
+        </div>
+        <div className="justify-self-end flex items-center">
+          {!hasEnvVars ? (
+            <EnvVarWarning />
+          ) : (
+            <>
+              <NotificationBell />
+              <AuthButton />
+            </>
+          )}
         </div>
       </nav>
     </header>
--- components/search.tsx
+++ components/search.tsx
@@ -9,7 +9,7 @@ import { Kbd } from "@/components/ui/kbd"
 
 export function Search() {
   return (
-    <div className="flex w-full max-w-xs flex-col gap-6">
+    <div className="flex w-full max-w-2xl flex-col gap-6">
       <InputGroup>
         <InputGroupInput placeholder="Search..." />
         <InputGroupAddon>
