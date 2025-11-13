1ba031ea9a5f145a1fcfde28e947c12651e14c0c
rmunoz392 <munozrenz12@gmail.com>
2025-11-12 21:58:09 -0600

    feat: create a button to redirect to alert page

--- components/alert-button.tsx
+++ components/alert-button.tsx
@@ -1,13 +1,12 @@
-import { PlusIcon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 
 export function AlertButton() {
   return (
-    <Link href="/protected/alerts"> 
-    <button className="p-2 hover:bg-muted bg-emerald-600 rounded-md">
-        <PlusIcon className="inline-block mr-2"/>Create Alert
-    </button>
-    </Link>
+    <div>
+      <Link href="/alert"> 
+      <Button className="p-2 bg-emerald-600 rounded-md">Alerts</Button>
+      </Link>
+    </div>
   );
 }
