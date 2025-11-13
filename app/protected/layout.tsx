af2548c0d98e2fab44d7416e73405a6e001b9de2
rmunoz392 <munozrenz12@gmail.com>
2025-11-12 20:44:10 -0600

    refactor: move create alert button into a component from protected

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -24,6 +24,7 @@ export default function ProtectedLayout({
               </div>)}
           </div>
         </nav>
+        
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           {children}
         </div>
---
+++ components/create-alert-button.tsx
@@ -0,0 +1,5 @@
+import { Link, PlusIcon } from "lucide-react";
+
+<Link href="/protected/alerts"> 
+<button className="p-2 hover:bg-muted bg-emerald-600 rounded-md"><PlusIcon className="inline-block mr-2" />Create new alert</button>
+</Link>
