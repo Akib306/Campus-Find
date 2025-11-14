28cab77290bc73fc0bff406cc017711b51d44134
rmunoz392 <munozrenz12@gmail.com>
2025-11-14 17:53:06 -0600

    refactor: remove unused import element

--- components/create-alert-form.tsx
+++ components/create-alert-form.tsx
@@ -12,7 +12,7 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useState, useEffect, JSX } from "react";
+import { useState, JSX } from "react";
 
 export function CreateAlertForm(): JSX.Element {
     const [category, setCategory] = useState("");
