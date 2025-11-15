ed8004cfe3361a119a375aa8e17b3bb9d3f03793
rmunoz392 <munozrenz12@gmail.com>
2025-11-14 20:01:09 -0600

    feat: create a modal to contain alert list and alert creation

--- components/alert-modal.tsx
+++ components/alert-modal.tsx
@@ -21,9 +21,13 @@ export function AlertModal() {
                 <Button type="button" size="sm" className="text-sm">See Alerts</Button>
             </DialogTrigger>
 
-            <DialogContent className="sm:max-w-[600px]">
-                <CreateAlertForm />
-                <AlertList />
+            <DialogContent className="sm:max-w-[650px] flex flex-shrink-0">
+                <div className="w-[220px] flex-shrink-0">
+                    <CreateAlertForm />
+                </div>
+                <div className="flex-1 min-w-0 pr-4">
+                    <AlertList /> 
+                </div>
             </DialogContent>
         </Dialog>
     )
